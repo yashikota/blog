063f705a9e5e97ab5783de7ba9509763b8c37b8e
kota <52403688+yashikota@users.noreply.github.com>
2022-03-22 11:48:18 +0900

    誤字修正

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,8 +12,8 @@ const projectsData = [
     href: 'https://syllabus.oit.yashikota.com',
   },
   {
-    title: 'OIT 単位取得率チェッカー',
-    description: `大阪工業大学の全学部・大学院に対応したシラバス閲覧アプリ。インクリメンタルサーチやフィルタリング、複数の年度にも対応`,
+    title: 'OIT単位取得率チェッカー',
+    description: `2018年度以降の授業アンケート結果・単位取得率が確認できるアプリ。閲覧には学内のGoogleアカウントが必要`,
     imgSrc: '/static/images/syllabus.webp',
     href: 'https://tani.oit.yashikota.com',
   },
