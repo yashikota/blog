780ffdf863fed0523edb29fdcd0ee65a058b4b5e
kota <52403688+yashikota@users.noreply.github.com>
2022-03-22 11:40:48 +0900

    プロジェクトの追加

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,13 +3,19 @@ const projectsData = [
     title: 'OIT Tools',
     description: `OIT Toolsの各アプリケーションへのリンクや大学より配信されるニュースを表示`,
     imgSrc: '/static/images/tools.webp',
-    href: 'https://tools.oit.yashikota.com',
+    href: 'https://oit.yashikota.com',
   },
   {
     title: 'OIT Syllabus App',
     description: `大阪工業大学の全学部・大学院に対応したシラバス閲覧アプリ。インクリメンタルサーチやフィルタリング、複数の年度にも対応`,
     imgSrc: '/static/images/syllabus.webp',
     href: 'https://syllabus.oit.yashikota.com',
+  },
+  {
+    title: 'OIT 単位取得率チェッカー',
+    description: `大阪工業大学の全学部・大学院に対応したシラバス閲覧アプリ。インクリメンタルサーチやフィルタリング、複数の年度にも対応`,
+    imgSrc: '/static/images/syllabus.webp',
+    href: 'https://tani.oit.yashikota.com',
   },
 ]
 
