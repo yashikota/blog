44fc717a95208cc36f70a9e6bad6226c6ccd32de
kota <52403688+yashikota@users.noreply.github.com>
2022-07-29 09:03:58 +0900

    update: projectsの更新

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,22 +1,22 @@
 const projectsData = [
   {
-    title: 'OIT Tools',
-    description: `OIT Toolsの各アプリケーションへのリンクや大学より配信されるニュースを表示`,
-    imgSrc: '/static/images/tools.webp',
-    href: 'https://oit.yashikota.com',
-  },
-  {
-    title: 'OIT Syllabus App',
-    description: `大阪工業大学の全学部・大学院に対応したシラバス閲覧アプリ。インクリメンタルサーチやフィルタリング、複数の年度にも対応`,
+    title: 'OITシラバスアプリ',
+    description: `大阪工業大学の全学部・大学院に対応したシラバス閲覧アプリ。インクリメンタルサーチやフィルタリング、複数の年度に対応`,
     imgSrc: '/static/images/syllabus.webp',
     href: 'https://syllabus.oit.yashikota.com',
   },
   // {
-  //   title: 'OIT単位取得率チェッカー',
-  //   description: `2018年度以降の授業アンケート結果・単位取得率が確認できるアプリ。閲覧には学内のGoogleアカウントが必要`,
-  //   imgSrc: '/static/images/syllabus.webp',
-  //   href: 'https://tani.oit.yashikota.com',
+  //   title: 'JANCodeジェネレーター',
+  //   description: `JANCode(Japanese Article Number Code)を生成するジェネレーター`,
+  //   imgSrc: '/static/images/jancode.webp',
+  //   href: 'https://pypi.org/project/jancode',
   // },
+  {
+    title: 'Aozora Markov Bot',
+    description: `青空文庫 (www.aozora.gr.jp) のアクセスランキング上位500位の作品からマルコフ連鎖により生成された文章を1時間に1回ツイートします`,
+    imgSrc: '/static/images/aozora-markov.webp',
+    href: 'https://twitter.com/AozoraMarkov',
+  },
 ]
 
 export default projectsData
